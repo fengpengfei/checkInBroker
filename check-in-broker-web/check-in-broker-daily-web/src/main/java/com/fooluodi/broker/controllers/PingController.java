6516169d526826ed86f096a96f24f1123b30adff
diwa <di@diwas-MBP.lan>
2016-08-07 00:38:44 +0800

    add frame workk

--- check-in-broker-web/check-in-broker-daily-web/src/main/java/com/fooluodi/broker/controllers/PingController.java
+++ check-in-broker-web/check-in-broker-daily-web/src/main/java/com/fooluodi/broker/controllers/PingController.java
@@ -1,5 +1,6 @@
 package com.fooluodi.broker.controllers;
 
+import com.fooluodi.broker.framework.WebAPIBaseController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by di on 6/8/2016.
  */
 @RestController(value = "/")
-public class PingController {
+public class PingController extends WebAPIBaseController{
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
         return "hi!";
